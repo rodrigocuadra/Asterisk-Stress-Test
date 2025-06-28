aa375bf0deb02ebc6d5343996aceee87d1509d2a
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-06-28 14:54:02 -0600

    Create state.py

---
+++ monitor/state.py
@@ -0,0 +1,13 @@
+  GNU nano 7.2                                                                                                                                 state.py                                                                                                                                          
+test_state = {
+    "asterisk": {
+        "steps": [],
+        "exploded": False,
+        "explosion_data": None
+    },
+    "freeswitch": {
+        "steps": [],
+        "exploded": False,
+        "explosion_data": None
+    }
+}

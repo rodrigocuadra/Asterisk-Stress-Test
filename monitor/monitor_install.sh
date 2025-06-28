961c5e618ea6b3e959e4faad6c1e545478b5f78b
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-06-28 15:02:40 -0600

    Create monitor_install.sh

---
+++ monitor/monitor_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt update && apt install -y python3 python3-pip
+pip3 install --break-system-packages fastapi uvicorn pydantic aiofiles python-multipart websockets requests
+
+mkdir /opt/stresstest_monitor
+
+wget -O /opt/stresstest_monitor/main.py  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/main.py
+wget -O /opt/stresstest_monitor/state.py  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/state.py
+wget -O /opt/stresstest_monitor/ws_manager.py  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/ws_manager.py
+
+mkdir /var/www/stresstest_monitor
+wget -O /var/www/stresstest_monitor/index.html  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/index.html
+
+wget -O /etc/nginx/sites-available/stresstest_monitor  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/stresstest_monitor
+ln -s /etc/nginx/sites-available/stresstest_monitor /etc/nginx/sites-enabled/
+nginx -t
+rm /etc/nginx/sites-enabled/default
+systemctl restart nginx
+
+wget -O /etc/systemd/system/stresstest-api.service  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/stresstest-api.service
+
+systemctl daemon-reload
+systemctl enable stresstest-api
+systemctl start stresstest-api

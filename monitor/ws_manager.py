b0435c95762666d2d77c50c157171e114695200d
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-06-30 15:54:06 -0600

    Update ws_manager.py

--- monitor/ws_manager.py
+++ monitor/ws_manager.py
@@ -1,23 +1,27 @@
-from fastapi import WebSocket
+from typing import List
+import websockets
 
 class WebSocketManager:
     def __init__(self):
-        self.active = []
+        self.active_connections: List[websockets.WebSocketServerProtocol] = []
 
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active.append(websocket)
+    async def connect(self, ws: websockets.WebSocketServerProtocol):
+        self.active_connections.append(ws)
+        print(f"New WebSocket connection added. Total connections: {len(self.active_connections)}")
 
-    async def disconnect(self, websocket: WebSocket):
-        if websocket in self.active:
-            self.active.remove(websocket)
+    async def disconnect(self, ws: websockets.WebSocketServerProtocol):
+        self.active_connections.remove(ws)
+        print(f"WebSocket connection removed. Total connections: {len(self.active_connections)}")
 
     async def broadcast(self, message: dict):
-        for ws in self.active:
+        if not self.active_connections:
+            print("No active WebSocket connections to broadcast to")
+            return
+        for connection in self.active_connections:
             try:
-                await ws.send_json(message)
-            except:
-                pass
+                await connection.send_json(message)
+                print(f"Message broadcasted to connection: {message}")
+            except Exception as e:
+                print(f"Failed to send message to a connection: {e}")
 
-# Este objeto es el que se importa en main.py
 manager = WebSocketManager()

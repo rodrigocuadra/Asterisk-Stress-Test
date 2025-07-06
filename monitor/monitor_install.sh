63fa968a0500daeb855d8e09b59ea7b9b6e3e18c
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-07-05 18:27:09 -0600

    Update monitor_install.sh

--- monitor/monitor_install.sh
+++ monitor/monitor_install.sh
@@ -18,17 +18,21 @@ wget https://cdn.jsdelivr.net/npm/xterm@5.3.0/lib/xterm.min.js -O /var/www/stres
 wget https://cdn.jsdelivr.net/npm/xterm-addon-fit@0.7.0/lib/xterm-addon-fit.min.js -O /var/www/stresstest_monitor/static/js/xterm-addon-fit.min.js
 wget https://cdn.jsdelivr.net/npm/chart.js -O /var/www/stresstest_monitor/static/js/chart.umd.min.js
 wget https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/dist/confetti.browser.min.js -O /var/www/stresstest_monitor/static/js/confetti.min.js
+wget https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/static/js/app.js -O /var/www/stresstest_monitor/static/js/app.js
 
 # CSS
 wget https://cdn.jsdelivr.net/npm/xterm@5.3.0/css/xterm.css -O /var/www/stresstest_monitor/static/css/xterm.css
+wget https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/static/css/styles.css -O /var/www/stresstest_monitor/static/css/tyles.css
 
+# Audios
 wget -O /var/www/stresstest_monitor/explosion.mp3  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/explosion.mp3
 wget -O /var/www/stresstest_monitor/winner.mp3  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/winner.mp3
 
-
+# web
 wget -O /var/www/stresstest_monitor/index.html  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/index.html
 wget -O /var/www/stresstest_monitor/favicon.ico  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/favicon.ico
 
+# App
 wget -O /etc/nginx/sites-available/stresstest_monitor  https://raw.githubusercontent.com/rodrigocuadra/Asterisk-Stress-Test/refs/heads/main/monitor/stresstest_monitor
 ln -s /etc/nginx/sites-available/stresstest_monitor /etc/nginx/sites-enabled/
 nginx -t

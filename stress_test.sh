be201474e0d80c7936ab797e8be8e537b9f286b0
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2025-07-08 08:45:42 -0600

    Update stress_test.sh

--- stress_test.sh
+++ stress_test.sh
@@ -238,7 +238,7 @@ else
 fi
 
 test_type="asterisk"
-info_url="${web_notify_url_base}/api/info"
+info_url="${web_notify_url_base}/api/system_info"
 progress_url="${web_notify_url_base}/api/progress"
 explosion_url="${web_notify_url_base}/api/explosion"
 
